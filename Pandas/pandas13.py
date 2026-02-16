c9e5a58cda8b0bcb607cf1367161e5094ed368cb
Mohit pal <pal001768@gmail.com>
2026-02-16 21:47:36 +0530

    Create pandas13.py

---
+++ Pandas/pandas13.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.to_string()) 
+
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df) 
+
+print(pd.options.display.max_rows) 
